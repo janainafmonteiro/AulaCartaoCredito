b52a6292e64cf6df389081e77c0da335f5b8064d
janainafmonteiro <janainaferreiramonteiro@hotmail.com>
2024-10-19 13:13:28 -0300

    feat: adicionei gets e sets

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -2,12 +2,14 @@
 package IntroducaoPOO;
 
 public class CartaoDeCredito {
-    String numero;
-    String nomeTitular;
-    String cpfTitular;
-    float limite;
-    float saldo;
+    private String numero;
+    private String nomeTitular;
+    private String cpfTitular;
+    private float limite;
+    private float saldo;
     
+    public CartaoDeCredito(){
+    }
     public CartaoDeCredito(String numero, String nomeTitular, String cpfTitular, float limite){
         this.numero = numero;
         this.nomeTitular = nomeTitular;
@@ -15,12 +17,43 @@ public class CartaoDeCredito {
         this.limite = limite;
         this.saldo=limite;
     }
-    public float consultarSaldo(){
-        return this.saldo;
+    public void inicializarSaldo(){
+        this.saldo= this.limite;
     }
-        
-    public float consultarLimite(){
+    public String getNumero(){
+        return this.numero;
+    }
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+    public String getNomeTitular(){
+        return this.nomeTitular;
+    }
+    public void setNomeTitular(String nome){
+        this.nomeTitular=nome;
+    }
+    public String getCpfTitular(){
+        return this.cpfTitular;
+    }  
+    public void setCpfTitular(String cpf){
+        this.cpfTitular = cpf;
+    } 
+    public float getSaldo(){
+        return this.saldo;
+    }   
+    public void setSaldo(float valor){
+        if(valor<=this.saldo){
+            this.saldo -= valor;
+            System.out.println("Transação realizada com sucesso");
+        }else{
+            System.out.println("Saldo insuficiente");
+        }
+    }
+    public float getLimite(){
       return this.limite; 
+    } 
+    public void setLimite(float limite){
+      this.limite = limite; 
     } 
     
     public void realizarTransacao(float valor){
--- Principal.java
+++ Principal.java
@@ -19,23 +19,33 @@ public class Principal{
         cpfTitular = entrada.nextLine();
         System.out.println("Escreva seu limite: ");
         limite = entrada.nextFloat();
-        CartaoDeCredito cartao = new CartaoDeCredito(numero, nomeTitular, cpfTitular, limite);
+        CartaoDeCredito cartao = new CartaoDeCredito();
+        cartao.setNumero(numero);
+        cartao.setNomeTitular(nomeTitular);
+        cartao.setCpfTitular(cpfTitular);
+        cartao.setLimite(limite);
+        cartao.inicializarSaldo();
        do{
             
-        System.out.println("O que deseja fazer: \n1. Realizar Transação\n2.Consultar Limite\n3.Consultar Saldo");
+        System.out.println("O que deseja fazer: \n1. Realizar Transação\n2.Consultar Limite\n3.Consultar Saldo\n4.Alterar Limite");
         op = entrada.nextInt();
         switch(op){
             case 1: 
                 System.out.println("Qual valor da transação?");
                 float valor = entrada.nextFloat();
-                cartao.realizarTransacao(valor);
+                cartao.setSaldo(valor);
                 break;
             case 2:           
-                System.out.println(cartao.consultarLimite());
+                System.out.println(cartao.getLimite());
                 break;
             case 3:               
-                System.out.println(cartao.consultarSaldo());
+                System.out.println(cartao.getSaldo());
                 break;
+            case 4:
+                System.out.println("Escreva seu limite: ");
+                limite = entrada.nextFloat();
+                cartao.setLimite(limite);
+                System.out.println("Limite alterado com sucesso!");
             default:
                 break;
        }
